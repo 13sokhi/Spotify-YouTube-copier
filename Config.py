82c7ba01275c69902a329dfcf2edaf68dfe2a09c
13sokhi <manparveshsinghsokhi@gmail.com>
2025-01-21 22:31:15 -0500

    Create Config.py
    added file so users can enter their account details to run program

---
+++ Config.py
@@ -0,0 +1,14 @@
+# youtube credentials
+youtube_client_secret_json_path = "" # absolute path of downloaded client secret JSON file here
+youtube_api_key = '' # your YouTube API key here
+
+# spotify credentials
+spotify_client_id = '' # your Spotify client ID here
+spotify_client_secret = '' # your Spotify client secret here
+spotify_redirect_uri = '' # your Spotify redirect URI here
+
+# Spotify request endpoints
+user_profile_endpoint = 'https://api.spotify.com/v1/me'
+base_authorization_url = 'https://accounts.spotify.com/authorize'
+token_url = 'https://accounts.spotify.com/api/token'
+base_youtube_search_url = 'https://www.youtube.com/results?search_query='
